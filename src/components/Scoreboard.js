35b68e8f5ead021dfe3d632ad2ca75a8e4034d43
nathangappy <nathan.gappy@gmail.com>
2021-12-20 22:31:45 -0500

    added time, score, etc to scoreboard

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import { GetScoreboard } from '../helpers/GetScoreboard';
+import { FormatQuarter } from '../helpers/FormatQuarter';
 
 const Scoreboard = () => {
   const [scoreboard, setScoreboard] = useState('');
@@ -21,20 +22,42 @@ const Scoreboard = () => {
         <div className='scoreboard__list'>
           {scoreboard.map((game) => (
             <div key={game.gameId} className='scoreboard__item'>
+              {/* Visitor Team */}
               <div className='scoreboard__team'>
                 <img src={`images/${game.vTeam.triCode}.png`} alt='Visitor Logo' />
                 <p>{game.vTeam.triCode}</p>
-                <span>
-                  {game.vTeam.win} - {game.vTeam.loss}
-                </span>
+                {game.isGameActivated || game.endTimeUTC ? (
+                  <span>{game.vTeam.score}</span>
+                ) : (
+                  <span>
+                    {game.vTeam.win} - {game.vTeam.loss}
+                  </span>
+                )}
               </div>
               <div className='scoreboard__vs'>vs</div>
+              {/* Home Team */}
               <div className='scoreboard__team'>
                 <img src={`images/${game.hTeam.triCode}.png`} alt='Home Logo' />
                 <p>{game.hTeam.triCode}</p>
-                <span>
-                  {game.hTeam.win} - {game.hTeam.loss}
-                </span>
+                {game.isGameActivated || game.endTimeUTC ? (
+                  <span>{game.hTeam.score}</span>
+                ) : (
+                  <span>
+                    {game.hTeam.win} - {game.hTeam.loss}
+                  </span>
+                )}
+              </div>
+              {/* Game Time */}
+              <div className='scoreboard__time'>
+                {game.isGameActivated ? (
+                  <p className='scoreboard__timer'>{`${game.clock} - ${FormatQuarter(
+                    game.period.current
+                  )} Quarter`}</p>
+                ) : game.endTimeUTC ? (
+                  <p className='scoreboard__completed'>Final</p>
+                ) : (
+                  game.startTimeEastern
+                )}
               </div>
             </div>
           ))}
---
+++ src/helpers/FormatQuarter.js
@@ -0,0 +1,13 @@
+// Switch statement to return the formatted quarter if game is in progress
+export const FormatQuarter = (period) => {
+  switch (period) {
+    case 1:
+      return '1st';
+    case 2:
+      return '2nd';
+    case 3:
+      return '3rd';
+    case 4:
+      return '4th';
+  }
+};
